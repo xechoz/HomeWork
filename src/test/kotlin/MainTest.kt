0ed8931835e76c260fe4d7b3f29e65a6cb7a9292
xechoz <xechoz.zheng@gmail.com>
2021-02-02 00:34:40 +0800

    add search test

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,7 +1,8 @@
-import chunzhao.Week_02.topKFrequent
-import chunzhao.Week_02.topKFrequent2
-import chunzhao.Week_02.topKFrequent3
+import chunzhao.Week_02.*
+import chunzhao.Week_03.search
+import chunzhao.Week_03.search2
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class MainTest {
     @Test
@@ -52,5 +53,20 @@ class MainTest {
     @Test
     fun levelOrderTest() {
 
+    }
+
+    @Test
+    fun climStarsTest() {
+        val result = climbStairs(10)
+
+        assertEquals(5, result, "")
+    }
+
+    @Test
+    fun searchTest() {
+        val array = intArrayOf(5, 1, 3)
+        assertEquals(-1, search2(array, 0), "-1, 3")
+//        assertEquals(3, search2(array, 7))
+//        assertEquals(5, search2(array, 1))
     }
 }
